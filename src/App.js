3327f573deec90bf8bfb25d8f6ebcd70426e7366
Mervin John Tampus <mervinjohn.tampus@cit.edu>
2021-07-14 15:41:39 +0800

    v6.11 7/14/21 Terms of Service

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './Homepage.js';
 import RentalDashboard from './components/main-pages/react-js/RentalDashboard';
 import AboutUs from './components/main-pages/react-js/AboutUs';
 import LearnMore from './components/information-page/LearnMore'
+import TermsService from './components/information-page/TermsService'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Switch loaction={location} key={location.pathname}>
           <Route path = '/rental-dashboard'  component={RentalDashboard}/>
           <Route path = '/learn-more'  component={LearnMore}/>
+          <Route path = '/terms-of-service' component={TermsService}/>
           <Route path = '/about-us'  component={AboutUs}/>
           <Route path = '/'  component={Homepage}/>
 
