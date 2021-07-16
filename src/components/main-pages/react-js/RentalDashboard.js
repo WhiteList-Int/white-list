62d9b0a8acbd10c0150657a3a58fdf450c3d0eff
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2021-07-16 14:26:17 +0800

    v6.15beta Rental Dashboard Partial

--- src/components/main-pages/react-js/RentalDashboard.js
+++ src/components/main-pages/react-js/RentalDashboard.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import FilterSearchBar from '../../filter-search-bar/FilterSearchBar';
 import ratedStar from '../../images/star-rated.svg';
 import unratedStar from '../../images/star-unrated.svg';
 import Img from '../../images/white-list-text.svg';
-import SearchBar from '../../search-bar/SearchBar';
+import SearchBar from './../../search-bar/SearchBar';
+import rentData from './../comp/renData';
+import filterOptions from './../comp/filterOptions';
 import "../css/RentalDashboard.css";
-import rentData from '../comp/renData';
 
 const RentalDashboard = () => {
     return (
-        <>
-            <div className="rental-dashboard">
-                <div className="rental-dashboard-head">
-                    <img className = 'rentel-dashboard-head-img' src = {Img} alt="WhiteList"></img>
-                    <SearchBar placeholder="Enter Location/Type/Name: " data={rentData} />
-                </div>
-                <div className="rental-dashboard-filter-container">
-                </div>
-                <div className="rental-dashboard-main-container">
-
-                </div>
+        <div className="rental-dashboard">
+            <div className="rental-dashboard-head">
+                <img className = 'rentel-dashboard-head-img' src = {Img} alt="WhiteList"></img>
+                <SearchBar placeholder="Enter Location/Name: " data={rentData} />
             </div>
-        </>
+            <div className="rental-dashboard-filter-container">
+                
+            </div>
+            <div className="rental-dashboard-main-container">
+
+            </div>
+        </div>
     )
 }
 export default RentalDashboard;
