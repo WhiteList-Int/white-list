aa62ffa4ec81b3595ae1746d28c5cc7deb684d0b
Mervin John C. Tampus <79685609+jdesolate@users.noreply.github.com>
2021-07-18 01:57:50 +0800

    v1.1 7/18/21 Pending Connections Display

--- src/components/main-pages/react-js/OwnerDashboard.js
+++ src/components/main-pages/react-js/OwnerDashboard.js
@@ -2,9 +2,14 @@ import React from 'react'
 import '../css/OwnerDashboard.css'
 import NavbarAbout from '../../navbar-source/NavbarAbout';
 import Img from '../../images/house9.jpg';
+import Img2 from '../../images/house4.jpg';
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
 import CreateIcon from '@material-ui/icons/Create';
+import RemoveIcon from '@material-ui/icons/Remove';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import Footer from '../../footer-source/Footer'
 
 function OwnerDashboard() {
     return (
@@ -12,7 +17,6 @@ function OwnerDashboard() {
         <body className="owner-dashboard">
             <div className="owner-dashboard-title-container">
                 <h1 className="your-rental">Your Rental</h1>
-                
                 </div>
                 <div className="owner-dashboard-card">
                 <div className="owner-rental">
@@ -28,7 +32,7 @@ function OwnerDashboard() {
                             </p>
                             <div className="owner-description-icon-create">
                             <IconButton>
-                            <CreateIcon color="inherit" />
+                            <CreateIcon color="action" fontSize='large'/>
                         </IconButton>
                             </div>
                         </div>
@@ -36,7 +40,7 @@ function OwnerDashboard() {
                         <h2 >Pending Connections</h2>
                         <div className="owner-dash-icon-add">
                         <IconButton>
-                            <AddIcon />
+                            <AddIcon fontSize='large'/>
                         </IconButton>
                         </div>
                         </div>
@@ -44,15 +48,89 @@ function OwnerDashboard() {
                         <h2 >Archived Connections</h2>
                         <div className="owner-dash-icon-add">
                         <IconButton>
-                            <AddIcon />
+                            <AddIcon fontSize='large'/>
                         </IconButton>
                         </div>
                         </div>
                     </div>
                 </div>
+
+                <div className="owner-rental">
+                <img className="owner-rental-img" src={Img2} alt="owned-rental" />
+                    <div className="owner-dash-rental-detail-card">
+                        <div className="owner-dash-title-container">
+                            <h2>Juan Place</h2>
+                        </div>
+                            <div className="owner-dash-pending-container-active">
+                                <div className="owner-dash-pending-cont-row">
+                                    <h3 >Pending Connections</h3>
+                                    <div className="owner-dash-icon-minus">
+                                    <IconButton>
+                                        <RemoveIcon />
+                                    </IconButton>
+                                    </div>
+                                </div>
+                            <div className="owner-dash-pending-request">
+                                <div className="owner-dash-pending-name-cont">
+                                    <h3>Jolly Bhie</h3>
+                                    <div className="owner-dash-button-cont">
+                                        <div className="owner-dash-disapprove-cont">
+                                            <IconButton>
+                                                <CloseIcon fontSize='large'/>
+                                            </IconButton>
+                                        </div>
+                                        <div className="owner-dash-approve-cont">
+                                            <IconButton>
+                                                <CheckIcon fontSize='large' />
+                                            </IconButton>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="owner-dash-pending-cont-row">
+                                    <p>09123456789</p>
+                                    <p>jobee@yahoo.com</p>
+                                </div>
+                            </div>
+                            <div className="owner-dash-pending-request">
+                                <div className="owner-dash-pending-name-cont">
+                                    <h3>Mac Dunald</h3>
+                                    <div className="owner-dash-button-cont">
+                                        <div className="owner-dash-disapprove-cont">
+                                            <IconButton>
+                                                <CloseIcon fontSize='large'/>
+                                            </IconButton>
+                                        </div>
+                                        <div className="owner-dash-approve-cont">
+                                            <IconButton>
+                                                <CheckIcon color= 'action' fontSize='large' />
+                                            </IconButton>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="owner-dash-pending-cont-row">
+                                    <p>09123456789</p>
+                                    <p>mcdnald@yahoo.com</p>
+                                </div>
+                            </div>
+                            </div>
+                        <div className="owner-dash-archived-container">
+                            <h2 >Archived Connections</h2>
+                            <div className="owner-dash-icon-add">
+                                <IconButton>
+                                    <AddIcon />
+                                </IconButton>
+                            </div>
+                        </div>
+                        <button className="owner-dash-remove-rental">
+                            <h2>Remove Rental Property</h2>
+                        </button>
+                    </div>
+                </div>
+
+                <NavbarAbout />
             </div>
         </body>
-        <NavbarAbout />
+        <Footer />
         </>
     )
 }
