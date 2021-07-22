85951be26cc0964b2c83f825e489b3b459b969c6
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2021-07-22 18:26:19 +0800

    v6.65 07/21/21 Pseudo Data

---
+++ src/components/main-pages/comp/sampleData.js
@@ -0,0 +1,139 @@
+const sampleData = [
+    {
+        name:'Abaka Lodge Rooms',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Lodging House',
+        category:'lodginghouse',
+        address: 'Lahug, Cebu City',
+        stars: 5, 
+        key: 1
+    },
+    {
+        name:'Edaha Boarding House',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Boarding House',
+        category:'boardinghouse',
+        address: 'Mambaling, Cebu City',
+        stars: 5, 
+        key: 2
+    },
+    {
+        name:'Ilama Apartment',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Apartment',
+        category:'apartment',
+        address: 'Tisa, Cebu City',
+        stars: 2, 
+        key: 3
+    },
+    {
+        name:'Naupa Apartment',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Apartment',
+        category:'apartment',
+        address: 'Ramos, Cebu City',
+        stars: 5, 
+        key: 4
+    },
+    {
+        name:'Rasata Rental',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Rent House',
+        category:'renthouse',
+        address: 'Talisay City',
+        stars: 3, 
+        key: 5
+    },
+    {
+        name:'Juan Lodge Rooms',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Lodging House',
+        category:'lodginghouse',
+        address: 'Bulacao, Cebu City',
+        stars: 5,
+        key: 6
+    },
+    {
+        name:'Maria for Lease',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Lease',
+        category:'lease',
+        address: 'Lahug, Cebu City',
+        stars: 4, 
+        key: 7
+    },
+    {
+        name:'Carlos Hotel',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Hotel',
+        category:'hotel',
+        address: 'Bulacao, Cebu City',
+        stars: 2, 
+        key: 8
+    },
+    {
+        name:'Sibila Lodge Rooms',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Lodging House',
+        category:'lodginghouse',
+        address: 'Talisay City',
+        stars: 1, 
+        key: 9
+    }, 
+    {
+        name:'Jose Apartment',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Apartment',
+        category:'apartment',
+        address: 'Talisay City',
+        stars: 4, 
+        key: 10
+    }, 
+    {
+        name:'Felipe Lodge Rooms',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Lodging House',
+        category:'lodginghouse',
+        address: 'Mambaling, Cebu City',
+        stars: 3, 
+        key: 11
+    }, 
+    {
+        name:'Cardo Lodge Rooms',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Lodging House',
+        category:'lodginghouse',
+        address: 'Mambaling, Cebu City',
+        stars: 2, 
+        key: 12
+    },
+    {
+        name:'Dalisay Hotel',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Hotel',
+        category:'hotel',
+        address: 'Bulacao, Cebu City',
+        stars: 4, 
+        key: 13
+    },
+    {
+        name:'Elena Lodge Rooms',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Lodging House',
+        category:'lodginghouse',
+        address: 'Talisay City',
+        stars: 5, 
+        key: 14
+    },
+    {
+        name:'Sebastian Boarding House',
+        description: 'You can have a valuable time with yourself and your family if you stay here. Connect now!', 
+        categoryName:'Boarding House',
+        category:'boardinghouse',
+        address: 'Bulacao, Cebu City',
+        stars: 3, 
+        key: 15
+    }
+];
+
+export default sampleData;
