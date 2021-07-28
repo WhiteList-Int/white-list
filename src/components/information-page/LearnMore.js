97798bbd5c3d538389e9d011feed499ad3985b71
Mervin John C. Tampus <79685609+jdesolate@users.noreply.github.com>
2021-07-29 02:03:26 +0800

    v3.0 07/29/21 Learn More Complete

--- src/components/information-page/LearnMore.js
+++ src/components/information-page/LearnMore.js
@@ -3,8 +3,6 @@ import Img1 from '../images/stock1.jpg';
 import Img2 from '../images/stock2.jpg';
 import Img3 from '../images/stock3.jpg';
 import Img4 from '../images/stock4.jpg';
-import Img5 from '../images/stock5.jpg';
-import Img6 from '../images/stock6.jpg';
 import Footer from '../essentials/Footer'
 import NavbarFixed from '../essentials/NavbarFixed';
 import { motion } from 'framer-motion';
@@ -28,28 +26,34 @@ function LearnMore() {
                     transition={transitions.tweenEaseOutFast}
                 >
                 <div className="learn-more-container-section">
-                    <div className="learn-more-container-section-1">
-                        <img src={Img1} alt="" />
-                        <div className="learn-more-container-section-overlay">
-                            <h1>Host your Rental <br/> with <span>WhiteList</span></h1>
+                    <div className="learn-more-container-section-top">
+                        <div className="learn-more-container-section-top-cont">
+                            <div className="learn-more-container-section-top-cont-overlay">
+                            <h1><strong>Host</strong> <br/> your <br/>Rental <br/> with White<span>List</span></h1>
+                            </div>
+                            <img src={Img1} alt="" />
                         </div>
                     </div>
-                    <div className="learn-more-container-section-2">
-                        <h2>We'll help you gain new connections</h2>
-                        <div className="learn-more-container-section-2-grid">
-                        
-                                <img src={Img2} alt="" />
-                          
-                            
-                                <img src={Img3} alt="" />
-                            
-                           
-                                <img src={Img4} alt="" />
+                    <div className="learn-more-container-section-mid">
+                        <center><h2>We'll help you gain new connections</h2></center>
+                    </div>
+                    <div className="learn-more-container-section-bottom">
+                        <div className="learn-more-container-section-bottom-grid">
+                            <img src={Img2} alt="" />  
+                            <img src={Img3} alt="" /> 
+                            <img src={Img4} alt="" />
+                        </div> 
+                    </div>
+
+                    <div className="learn-more-container-section-text">
+                            <h3>Join the WhiteList community and list your property with ease.</h3>
+                            <a href='https://forms.gle/DHS7Ww1jNJAgxaLC7' className='card-2-sign-up-btn' >Host Rental</a>
                            
                         </div>
-                    </div>
                 </div>
-               
+
+
+
                 {/* <img src={Img2} alt="stock photo2" />    
                 <img src={Img3} alt="stock photo3" />    
                 <img src={Img4} alt="stock photo4" />    
